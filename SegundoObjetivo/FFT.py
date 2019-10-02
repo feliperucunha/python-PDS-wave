1a152bbe5d6796793fc1ed24b1201caf933aae94
VicLopes <viclopes2621@gmail.com>
2019-10-02 15:57:52 -0300

    Versatile pathing for all code purposes, along with some minor changes to the way things are calculated on modulacao.py.

--- SegundoObjetivo/FFT.py
+++ SegundoObjetivo/FFT.py
@@ -6,7 +6,24 @@ import time, sys
 from pygame import mixer
 from numpy.fft import fft
 
-(freq,sig) = wav.read("c:/Users/DraKo/Documents/PDS/SegundoObjetivo/audio1.wav")
+
+def addressReader():
+    address = input("Please, insert the name of the file, or the full path if it isn't on the same folder.")
+    if address[1] == ':':
+        final_address = address
+    else:
+        final_address = "./" + address # If the input isn't the full path to the file, it assumes to be in the same folder.
+    
+    if address[len(address)-2:] == ".wav": # Adds .wav in case it wasn't put in beforehand.
+        final_address = final_address
+    else:
+        final_address = final_address + ".wav"
+
+    return final_address
+
+address = addressReader()
+
+(freq,sig) = wav.read(address)
 n = len(sig)
 Fs = freq  # sampling rate
 Ts = 1.0/Fs  # sampling interval
@@ -17,7 +34,6 @@ frq = k/T
 frq = frq[range(n//2)]
 sigFFT = fft(sig)/n
 sigFFT = sigFFT[range(n//2)]
-
 
 fig, ax = plt.subplots(2, 1)
 ax[0].plot(sig)
--- SegundoObjetivo/modulacao.py
+++ SegundoObjetivo/modulacao.py
@@ -5,9 +5,25 @@ import matplotlib.pyplot as plt
 import time, sys
 from pygame import mixer
 
-(freq,sig) = wav.read("c:/Users/DraKo/Documents/PDS/SegundoObjetivo/audio1.wav")
-Fs = 31123.25
-n = np.arange(0, 8, 1/Fs)
+def addressReader():
+    address = input("Please, insert the name of the file, or the full path if it isn't on the same folder.")
+    if address[1] == ':':
+        final_address = address
+    else:
+        final_address = "./" + address # If the input isn't the full path to the file, it assumes to be in the same folder.
+    
+    if address[len(address)-2:] == ".wav": # Adds .wav in case it wasn't put in beforehand.
+        final_address = final_address
+    else:
+        final_address = final_address + ".wav"
+
+    return final_address
+
+address = addressReader()
+
+(freq,sig) = wav.read("address")
+Fs = freq
+n = np.arange(0, 1/Fs, 1/Fs)
 Fc =6
 Ac = 3
 carrier = Ac*np.cos(2*np.pi*Fc*n)
